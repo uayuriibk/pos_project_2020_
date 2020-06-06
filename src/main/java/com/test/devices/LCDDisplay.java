49bacb626dc4c10287f02bd7efdd750c59700f93
yuriybk <email2yurii@gmail.com>
2020-06-06 19:27:47 +0300

    minor

--- src/main/java/com/test/devices/LCDDisplay.java
+++ src/main/java/com/test/devices/LCDDisplay.java
@@ -3,6 +3,8 @@ package com.test.devices;
 public class LCDDisplay implements IDisplay {
 
     public void showMessage(String message){
+
         System.out.println("LCDDisplay: " + message);
+
     }
 }
--- src/main/java/com/test/devices/LaserPrinter.java
+++ src/main/java/com/test/devices/LaserPrinter.java
@@ -3,6 +3,8 @@ package com.test.devices;
 public class LaserPrinter implements IPrinter {
 
     public void printMessage(String message){
+
         System.out.println("LaserPrinter :" + message);
+
     }
 }
