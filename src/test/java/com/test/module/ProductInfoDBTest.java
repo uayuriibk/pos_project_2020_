fe18719804a970dba8258d65df69ed234c7f2b8f
yuriybk <email2yurii@gmail.com>
2020-06-08 23:43:23 +0300

    Preliminary version of test for ProductsInfo mock db has been added

---
+++ src/test/java/com/test/module/ProductInfoDBTest.java
@@ -0,0 +1,32 @@
+package com.test.module;
+
+import com.test.productsprices.Product;
+import com.test.productsprices.ProductsInfoDB;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
+
+public class ProductInfoDBTest {
+
+    private ProductsInfoDB productsInfoDB;
+
+    @Before
+    public void setUp(){
+        productsInfoDB = new ProductsInfoDB();
+    }
+
+    @Test
+    public void testProductAdding(){
+        Product productMock = Mockito.mock(Product.class);
+        when(productMock.getCode()).thenReturn("testCode");
+
+        productsInfoDB.addProduct(productMock);
+
+        int expectedSavedProductsCount = 1;
+        int actualSavedProductsCount = productsInfoDB.getProductsCodesMap().size();
+        Assert.assertEquals(expectedSavedProductsCount, actualSavedProductsCount);
+    }
+}
